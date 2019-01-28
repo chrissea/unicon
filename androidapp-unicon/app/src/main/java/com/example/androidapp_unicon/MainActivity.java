8c8d838dfbab166b6cf58682c8c53b499da9f974
chrissea <chrix@mit.edu>
2019-01-28 01:03:24 -0500

    clean empty app actually added this time

---
+++ androidapp-unicon/app/src/main/java/com/example/androidapp_unicon/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.androidapp_unicon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
