c438b01048b1bb74a03861ee7852b857b83614ff
wala-a <walkhanaizi@gmail.com>
2019-01-30 14:40:22 -0500

    adding PSoC things

---
+++ PSoC/unicon_workspace/PSoC_Unicon.cydsn/Generated_Source/PSoC5/ShiftReg_rev_PM.c
@@ -0,0 +1,139 @@
+/*******************************************************************************
+* File Name: ShiftReg_rev_PM.c
+* Version 2.30
+*
+* Description:
+*  This file provides the API source code for sleep mode support for Shift
+*  Register component.
+*
+* Note:
+*
+********************************************************************************
+* Copyright 2008-2013, Cypress Semiconductor Corporation.  All rights reserved.
+* You may use this file only in accordance with the license, terms, conditions,
+* disclaimers, and limitations in the end user license agreement accompanying
+* the software package with which this file was provided.
+*******************************************************************************/
+
+#include "ShiftReg_rev.h"
+
+static ShiftReg_rev_BACKUP_STRUCT ShiftReg_rev_backup =
+{
+    ShiftReg_rev_DISABLED,
+
+    ((uint8) ShiftReg_rev_DEFAULT_A0),
+    ((uint8) ShiftReg_rev_DEFAULT_A1),
+
+    #if(CY_UDB_V0)
+        ((uint8) ShiftReg_rev_INT_SRC),
+    #endif /* (CY_UDB_V0) */
+};
+
+
+/*******************************************************************************
+* Function Name: ShiftReg_rev_SaveConfig
+********************************************************************************
+*
+* Summary:
+*  Saves Shift Register configuration.
+*
+* Parameters:
+*  None.
+*
+* Return:
+*  None.
+*
+* Reentrant:
+*  No.
+*
+*******************************************************************************/
+void ShiftReg_rev_SaveConfig(void) 
+{
+    /* Store working registers A0 and A1 */
+    ShiftReg_rev_backup.saveSrA0Reg   = CY_GET_REG8(ShiftReg_rev_SHIFT_REG_LSB_PTR);
+    ShiftReg_rev_backup.saveSrA1Reg   = CY_GET_REG8(ShiftReg_rev_SHIFT_REG_VALUE_LSB_PTR);
+
+    #if(CY_UDB_V0)
+        ShiftReg_rev_backup.saveSrIntMask = ShiftReg_rev_SR_STATUS_MASK;
+    #endif /* (CY_UDB_V0) */
+}
+
+
+/*******************************************************************************
+* Function Name: ShiftReg_rev_RestoreConfig
+********************************************************************************
+*
+* Summary:
+*  Restores Shift Register configuration.
+*
+* Parameters:
+*  None.
+*
+* Return:
+*  None.
+*
+*******************************************************************************/
+void ShiftReg_rev_RestoreConfig(void) 
+{
+    /* Restore working registers A0 and A1 */
+    CY_SET_REG8(ShiftReg_rev_SHIFT_REG_LSB_PTR, ShiftReg_rev_backup.saveSrA0Reg);
+    CY_SET_REG8(ShiftReg_rev_SHIFT_REG_VALUE_LSB_PTR, ShiftReg_rev_backup.saveSrA1Reg);
+
+    #if(CY_UDB_V0)
+        ShiftReg_rev_SR_STATUS_MASK = ((uint8) ShiftReg_rev_backup.saveSrIntMask);
+    #endif /* (CY_UDB_V0) */
+}
+
+
+/*******************************************************************************
+* Function Name: ShiftReg_rev_Sleep
+********************************************************************************
+*
+* Summary:
+*  Prepare the component to enter a Sleep mode.
+*
+* Parameters:
+*  None.
+*
+* Return:
+*  None.
+*
+* Reentrant:
+*  No.
+*
+*******************************************************************************/
+void ShiftReg_rev_Sleep(void) 
+{
+    ShiftReg_rev_backup.enableState = ((uint8) ShiftReg_rev_IS_ENABLED);
+
+    ShiftReg_rev_Stop();
+    ShiftReg_rev_SaveConfig();
+}
+
+
+/*******************************************************************************
+* Function Name: ShiftReg_rev_Wakeup
+********************************************************************************
+*
+* Summary:
+*  Restores and enables the user configuration.
+*
+* Parameters:
+*  None.
+*
+* Return:
+*  None.
+*
+*******************************************************************************/
+void ShiftReg_rev_Wakeup(void) 
+{
+    ShiftReg_rev_RestoreConfig();
+
+    if(0u != ShiftReg_rev_backup.enableState)
+    {
+        ShiftReg_rev_Enable();
+    }
+}
+
+
+/* [] END OF FILE */
---
+++ PSoC/unicon_workspace/PSoC_Unicon.cydsn/codegentemp/PSoC_Unicon_u.sdc
@@ -0,0 +1,3 @@
+# Component constraints for C:\Users\Walaa\Documents\PSoC Creator\unicon_workspace\PSoC_Unicon.cydsn\TopDesign\TopDesign.cysch
+# Project: C:\Users\Walaa\Documents\PSoC Creator\unicon_workspace\PSoC_Unicon.cydsn\PSoC_Unicon.cyprj
+# Date: Wed, 30 Jan 2019 01:46:44 GMT
